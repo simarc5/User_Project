ab9a6bc3aa5c58632d033be17b7727e2adcaa4f7
simarchhabra6 <simarchhabra5@gmail.com>
2019-01-23 11:16:48 -0500

    User Person Project

---
+++ NetBeansProjects/JavaLab1/src/Business/User.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author sahebsingh
+ */
+public class User {
+    private String name;
+    private int dob;
+    private int telNum;
+    private String email;
+    private int ssn;
+    private String linkedin;
+    private int vehicle;
+    private int licence;
+    private int bank;
+    private String image;
+    private String health;
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public int getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(int telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public int getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(int vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public int getLicence() {
+        return licence;
+    }
+
+    public void setLicence(int licence) {
+        this.licence = licence;
+    }
+
+    public int getBank() {
+        return bank;
+    }
+
+    public void setBank(int bank) {
+        this.bank = bank;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+   
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
